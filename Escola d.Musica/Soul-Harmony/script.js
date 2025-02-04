63b2d000cc3866eb1340892cc45148d0d00cb605
Nilsatehc <nilsasouza77@gmail.com>
2025-02-04 13:30:45 -0300

    site pronto

---
+++ Escola d.Musica/Soul-Harmony/script.js
@@ -0,0 +1,17 @@
+
+const title = document.getElementById("title");
+
+
+window.onload = () => {
+    
+    title.style.animation = "slideIn 1.5s forwards";
+};
+
+const cabecalho = document.getElementById("cabecalho");
+
+
+window.onload = () => {
+   
+    cabecalho.style.animation = "fadeInZoom 2s forwards";
+};
+      
